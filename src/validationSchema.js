594a5b6d0469ca8b9b626652b6382cc0b4f36091
Soumya Biswas <soumyabiet@gmail.com>
2020-09-27 19:36:04 +0530

    Validation message changed

--- src/validationSchema.js
+++ src/validationSchema.js
@@ -4,14 +4,14 @@ export const validationSchema = Yup.object({
     name:   Yup.string()
             .min(2,'Name must be at least 2 characters')
             .max(20, 'Name must be at most 20 characters')
-            .required('Name must not be empty'),
+            .required('Name can not be empty'),
     email:  Yup.string()
             .email('Enter valid email address')
-            .required('Email must not be empty'),
+            .required('Email can not be empty'),
     password: Yup.string()
             .min(6, 'Password must be at least 6 characters')
-            .required('Password must not be empty'),
+            .required('Password can not be empty'),
     confirmPassword: Yup.string()
             .oneOf([Yup.ref('password'), null], 'Passwords must match')
-            .required('Confirm password must not be empty')
+            .required('Confirm password can not be empty')
 })
