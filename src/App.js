11c460e4336437977ccce7c0846e402aa2215ce0
Soumya Biswas <soumyabiet@gmail.com>
2020-09-27 19:30:28 +0530

    Complete form validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Col, Button, FormGroup, FormLabel } from 'react-bootstrap';
+
+import { validationSchema } from "./validationSchema";
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+};
+
 function App() {
+  const [formValues, setFormValues] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Basic Form Validation using <code>Formik &amp; Yup</code>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {formValues ? (<p>
+          {formValues}
+        </p>) : null}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            setFormValues(JSON.stringify(values));
+          }}>
+          {
+            () => {
+              return (
+                <Form>
+                  <FormGroup>
+                    <Col sm={2}>
+                      <FormLabel>
+                        Name:
+                    </FormLabel>
+                    </Col>
+                    <Col sm={10}>
+                      <Field
+                        type="text"
+                        name="name"
+                      />
+                    </Col>
+                    <ErrorMessage name="name">{value => <Col sm={10}><small className="text-danger">{value}</small></Col>}</ErrorMessage>
+                  </FormGroup>
+                  <FormGroup>
+                    <Col sm={2}>
+                      <FormLabel>Email: </FormLabel>
+                    </Col>
+                    <Col sm={10}>
+                      <Field
+                        type="text"
+                        name="email"
+                      />
+                    </Col>
+                    <ErrorMessage name="email">{value => <Col sm={10}><small className="text-danger">{value}</small></Col>}</ErrorMessage>
+                  </FormGroup>
+                  <FormGroup>
+                    <Col sm={2}>
+                      <FormLabel>Password: </FormLabel>
+                    </Col>
+                    <Col sm={10}>
+                      <Field
+                        type="password"
+                        name="password"
+                      />
+                    </Col>
+                    <ErrorMessage name="password">{value => <Col sm={10}><small className="text-danger">{value}</small></Col>}</ErrorMessage>
+                  </FormGroup>
+                  <FormGroup>
+                    <Col sm={10}>
+                      <FormLabel>Confirm Password: </FormLabel>
+                    </Col>
+                    <Col sm={10}>
+                      <Field
+                        type="password"
+                        name="confirmPassword"
+                      />
+                    </Col>
+                    <ErrorMessage name="confirmPassword">{value => <Col sm={12}><small className="text-danger">{value}</small></Col>}</ErrorMessage>
+                  </FormGroup>
+                  <FormGroup>
+                    <Col>&nbsp;</Col>
+                    <Col sm={10}><Button name="submit" type="submit">Submit</Button></Col>
+                  </FormGroup>
+                </Form>
+              )
+            }
+          }
+
+        </Formik>
       </header>
     </div>
   );
